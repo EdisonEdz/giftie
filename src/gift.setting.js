fdad976ad42106063f5d318f55ab553307c71ba4
EdisonEdz <1187949325@qq.com>
2023-02-06 02:55:27 +0000

    Changes to be committed: 	modified:   package.json 	modified:   src/gift.setting.js

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -11,17 +11,17 @@ export const IS_SHOW_RESTART = false;
 // 首页配置
 export const homeConfig = {
   // 标题
-  title: '520送礼抽奖',
+  title: '情人节送礼抽奖',
   // 留言，数组的每一项代表一行
   messages: [
-    { key: 'a', wording: '这是第一行文案', },
-    { key: 's', wording: '这是第二行文案', },
-    { key: 'd', wording: '然后，这里是第三行文案', },
+    { key: 'a', wording: 'Hi 王珺', },
+    { key: 's', wording: '这次的礼物，用抽奖的形式', },
+    { key: 'd', wording: '把“选择权”交给你', },
   ],
   // 最终解释权归属人
-  owner: 'XXX',
+  owner: 'EdisonEdz',
   // 抽奖转盘的动画时间，单位毫秒
-  timeout: 5000,
+  timeout: 3000,
   // 指定赠送的礼物的索引（第几个礼物中奖），取值范围 0-8
   targetGiftIndex: null,
 };
